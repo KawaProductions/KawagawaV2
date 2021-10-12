05e8f82d445be2ebc18fa8bdc6765a52cb0534dd
Raoul <6421655+Raove@users.noreply.github.com>
2021-10-11 20:21:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-client.on("ready", () => {
-    console.log('Logged in as ${client.user.tag}!')
-})
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
 
-client.on("message", msg => {
-    if(msg.content === "ping"){
-        msg.reply("pong")
+const rest = new REST({ version: '9' }).setToken('token');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const {Client, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
     }
-})
+});
 
-client.login(process.env.TOKEN)
+client.login("NzExMDY3NjczNjg0ODY5MTgw.Xr9nYA.VRMvnNajJ7f7I8kBItJEC4Rwk4c");
